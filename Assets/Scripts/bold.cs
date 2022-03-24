ed9fae43d38a16be0d29a7e2b940004beaa25814
andersbundgaard <andersbundgaard.dk@gmail.com>
2022-03-24 11:30:24 +0100

    Bold

--- Assets/Scripts/bold.cs
+++ Assets/Scripts/bold.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private bool _isGrounded;
+    public int JumpStrength;
+    public int HighJump;
+    private Rigidbody2D RB;
     // Start is called before the first frame update
     void Start()
     {
-        
+       RB = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+ 
+        if(Input.GetKeyDown(KeyCode.UpArrow) ){
+            RB.AddForce(new Vector2(0, JumpStrength));
+        }
     }
 }
