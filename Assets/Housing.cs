9c20536c5907920fbed88a313df1deae09a34373
andersbundgaard <andersbundgaard.dk@gmail.com>
2022-04-28 12:08:31 +0200

    Levels

--- Assets/Housing.cs
+++ Assets/Housing.cs
@@ -13,6 +13,7 @@ public class Housing : MonoBehaviour
     {
         GameObject spawn = Instantiate(RundsavPrefab) as GameObject; //Laver en klon af prefab, som er typen gameobject
         spawn.transform.position = SpawnPoint.transform.position; // spawner saven ind på spawnpoints' plads
+        spawn.tag = "Dead";
         spawn.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0); //giver saven en hastighed
         Destroy(spawn, 3);// ødelægger saven ever 3 sekunder
         yield return new WaitForSeconds(SpawnInterval); //Stopper IEnumerator, og venter i x sekunder
